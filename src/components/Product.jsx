dfcd32b32428964c056c19038eebfb01dfb7c836
Toba <riemehasan06@gmail.com>
2026-01-05 01:53:48 +0600

    shop added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,8 +6,8 @@ import { HiOutlineRefresh } from "react-icons/hi";
 
 const Product = ({ productImg, badgeText, productTitle, productPrice }) => {
   return (
-    <div className="group mx-5">
-      <div className="relative bg-[#f3f3f3]">
+    <div className="group mx-3">
+      <div className="relative bg-[#f3f3f3] mt-5">
         <Image src={productImg} />
         <Badge badgeText={badgeText} className={"absolute top-5 left-5"} />
         <div className="bg-[#ffffff] p-6 opacity-0 group-hover:opacity-100 transition duration-300 bottom-0 left-0 absolute w-full">
@@ -28,8 +28,8 @@ const Product = ({ productImg, badgeText, productTitle, productPrice }) => {
         </div>
       </div>
       <Flex className={"justify-between mt-4"}>
-        <h3>{productTitle}</h3>
-        <h3>{productPrice}</h3>
+        <h3 className="font-DM text-[20px] font-bold text-deepBlack">{productTitle}</h3>
+        <h3 className="font-DM text-base text-[#767676]">{productPrice}</h3>
       </Flex>
     </div>
   );
--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -4,12 +4,22 @@ import Flex from "../Flex";
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
 import { FaCircle } from "react-icons/fa";
-import Image from "../Image";
-import gridOne from "../../assets/gridOne.png"
-import gridTwo from "../../assets/gridTwo.png"
-
+import { IoGridSharp } from "react-icons/io5";
+import { AiOutlineBars } from "react-icons/ai";
+import { BiSolidDownArrow } from "react-icons/bi";
+import Product from "../Product";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Shop = () => {
+  let [allData, setAllData] = useState([]);
+  useEffect(() => {
+    async function alldatas() {
+      let data = await axios.get("https://dummyjson.com/products");
+      setAllData(data.data.products);
+    }
+    alldatas();
+  }, []);
   return (
     <div>
       <Container>
@@ -27,7 +37,7 @@ const Shop = () => {
           {/* Flex left start  */}
 
           {/* Shop by Category part  */}
-          <div className="w-1/4">
+          <div className="w-[25%]">
             <h5 className="font-DM font-bold text-[20px] text-deepBlack py-10">
               Shop by Category
             </h5>
@@ -111,31 +121,25 @@ const Shop = () => {
             </h5>
             <div className="">
               <ul>
-                <div className="flex gap-x-4 items-center font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
-                  <li className="">
-                    <Link to={"/"}>Brand 1</Link>
-                  </li>
-                </div>
-                <div className="flex gap-x-4 items-center font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
-                  <li className="">
-                    <Link to={"/"}>Brand 2</Link>
-                  </li>
-                </div>
-                <div className="flex gap-x-4 items-center font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
-                  <li className="">
-                    <Link to={"/"}>Brand 3</Link>
-                  </li>
-                </div>
-                <div className="flex gap-x-4 items-center font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
-                  <li className="">
-                    <Link to={"/"}>Brand 4</Link>
-                  </li>
-                </div>
-                <div className="flex gap-x-4 items-center font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
-                  <li className="">
-                    <Link to={"/"}>Brand 5</Link>
-                  </li>
-                </div>
+                <li className="font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
+                  <Link to={"/"}>Brand 1</Link>
+                </li>
+
+                <li className="font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
+                  <Link to={"/"}>Brand 2</Link>
+                </li>
+
+                <li className="font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
+                  <Link to={"/"}>Brand 3</Link>
+                </li>
+
+                <li className="font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
+                  <Link to={"/"}>Brand 4</Link>
+                </li>
+
+                <li className="font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
+                  <Link to={"/"}>Brand 5</Link>
+                </li>
               </ul>
             </div>
             {/* Shop by Brand part  */}
@@ -145,41 +149,68 @@ const Shop = () => {
             </h5>
             <div className="">
               <ul>
-                <div className="flex gap-x-4 items-center font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
-                  <li className="">
-                    <Link to={"/"}>$0.00 - $9.99</Link>
-                  </li>
-                </div>
-                <div className="flex gap-x-4 items-center font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
-                  <li className="">
-                    <Link to={"/"}>$10.00 - $19.99</Link>
-                  </li>
-                </div>
-                <div className="flex gap-x-4 items-center font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
-                  <li className="">
-                    <Link to={"/"}>$20.00 - $29.99</Link>
-                  </li>
-                </div>
-                <div className="flex gap-x-4 items-center font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
-                  <li className="">
-                    <Link to={"/"}>$30.00 - $39.99</Link>
-                  </li>
-                </div>
-                <div className="flex gap-x-4 items-center font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
-                  <li className="">
-                    <Link to={"/"}>$40.00 - $69.99</Link>
-                  </li>
-                </div>
+                <li className="font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
+                  <Link to={"/"}>$0.00 - $9.99</Link>
+                </li>
+
+                <li className="font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
+                  <Link to={"/"}>$10.00 - $19.99</Link>
+                </li>
+
+                <li className="font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
+                  <Link to={"/"}>$20.00 - $29.99</Link>
+                </li>
+
+                <li className="font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
+                  <Link to={"/"}>$30.00 - $39.99</Link>
+                </li>
+
+                <li className="font-DM text-base text-[#767676] border-b border-b-[#D8D8D8] py-5">
+                  <Link to={"/"}>$40.00 - $69.99</Link>
+                </li>
               </ul>
             </div>
             {/* Shop by Price part  */}
           </div>
           {/* Flex left end  */}
           {/* Flex right start  */}
-          <div className="w-1/2">
-            <div className="flex items-center">
-              <Image src={gridOne}/>
-              <Image src={gridTwo}/>
+          <div className="w-[75%] py-10">
+            <div className="flex items-center justify-between mb-8">
+              <div className="flex items-center gap-x-6">
+                <IoGridSharp className="text-3xl hover:text" />
+                <AiOutlineBars className="text-3xl" />
+              </div>
+
+              <div className="flex items-center text-end gap-x-2 font-DM text-base text-[#767676] relative">
+                <h6>Sort by:</h6>
+                <input
+                  className="border border-[#767676] px-3 py-2 w-[239px]"
+                  type="text"
+                  placeholder="Featured"
+                />
+                <BiSolidDownArrow className="absolute top-3.5 right-3 text-sm" />
+              </div>
+              <div className="flex items-center gap-x-2 font-DM text-base text-[#767676] relative">
+                <h6>Show:</h6>
+                <input
+                  className="border border-[#767676] px-3 py-2 w-[239px]"
+                  type="text"
+                  placeholder="36"
+                />
+                <BiSolidDownArrow className="absolute top-3.5 right-3 text-sm" />
+              </div>
+            </div>
+            <div className="grid grid-cols-3">
+              {allData.map((item) => (
+                <div>
+                  <Product
+                    productImg={item.thumbnail}
+                    badgeText={item.rating}
+                    productTitle={item.title}
+                    productPrice={item.price}
+                  />
+                </div>
+              ))}
             </div>
           </div>
           {/* Flex right end  */}
