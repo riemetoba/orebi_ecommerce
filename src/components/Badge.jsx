defcad71432db521b79e1684707a9004276562c7
Toba <riemehasan06@gmail.com>
2026-06-30 22:19:47 +0600

    style: optimize product and badge responsiveness

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,7 +1,7 @@
 const Badge = ({ badgeText, className }) => {
   return (
     <div
-      className={`w-[92px] py-2.25 bg-deepBlack text-white px-7.5 font-bold text-sm font-DM ${className}`}
+      className={`w-auto sm:w-[92px] py-1 sm:py-2.25 bg-deepBlack text-white px-2.5 sm:px-7.5 font-bold text-[10px] sm:text-sm font-DM text-center ${className}`}
     >
       {badgeText}
     </div>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,7 +9,10 @@ const Product = ({ productImg, badgeText, productTitle, productPrice }) => {
     <div className="group mx-1.5 md:mx-3">
       <div className="relative bg-[#f3f3f3] mt-5 overflow-hidden">
         <Image src={productImg} className="w-full h-auto object-cover" />
-        <Badge badgeText={badgeText} className="absolute top-3 left-3 sm:top-5 sm:left-5 text-[10px] sm:text-sm px-2 sm:px-4 py-0.5 sm:py-1" />
+        <Badge
+          badgeText={badgeText}
+          className="absolute top-3 left-3 sm:top-5 sm:left-5 text-[10px] sm:text-sm px-2 sm:px-4 py-0.5 sm:py-1"
+        />
         <div className="bg-[#ffffff] p-3 sm:p-6 opacity-0 group-hover:opacity-100 transition duration-300 bottom-0 left-0 absolute w-full hidden sm:block">
           <Flex className="justify-end gap-x-3 items-center mb-1 sm:mb-2">
             <h3 className="font-DM text-xs sm:text-base text-[#767676]">
@@ -18,11 +21,15 @@ const Product = ({ productImg, badgeText, productTitle, productPrice }) => {
             <FaHeart className="text-xs sm:text-sm" />
           </Flex>
           <Flex className="justify-end gap-x-3 items-center mb-1 sm:mb-2">
-            <h3 className="font-DM text-xs sm:text-base text-[#767676]">Compare</h3>
+            <h3 className="font-DM text-xs sm:text-base text-[#767676]">
+              Compare
+            </h3>
             <HiOutlineRefresh className="text-xs sm:text-sm" />
           </Flex>
           <Flex className="justify-end gap-x-3 items-center">
-            <h3 className="font-DM text-xs sm:text-base font-bold">Add to Cart</h3>
+            <h3 className="font-DM text-xs sm:text-base font-bold">
+              Add to Cart
+            </h3>
             <FaShoppingCart className="text-xs sm:text-sm" />
           </Flex>
         </div>
