37bb9522826e9cf9da463a4e6f9ebe6f2e051f4b
Toba <riemehasan06@gmail.com>
2026-07-01 17:04:17 +0600

    style: make bestsellers section responsive

--- src/components/layouts/Bestsellers.jsx
+++ src/components/layouts/Bestsellers.jsx
@@ -9,8 +9,9 @@ import "slick-carousel/slick/slick.css";
 import Slider from "react-slick";
 import NextArrow from "../NextArrow";
 import PrevArrow from "../PrevArrow";
+
 const Bestsellers = () => {
-  var settings = {
+  const settings = {
     dots: false,
     infinite: true,
     speed: 500,
@@ -18,18 +19,44 @@ const Bestsellers = () => {
     slidesToScroll: 1,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      }
+    ]
   };
+
   return (
     <>
       <Container>
-        <h2 className="text-[24px] font-DM font-bold pb-10 lg:pb-[55px] lg:text-[39px] pt-25">
+        <h2 className="text-[24px] font-DM font-bold pb-10 lg:pb-[55px] lg:text-[39px] pt-25 px-4 md:px-0">
           Our Bestsellers
         </h2>
         {/* ========= */}
-        <div className="-mx-4">
+        <div className="mx-0 md:-mx-4 px-2 md:px-0 w-full overflow-hidden">
           <Slider {...settings}>
             {/* ======== */}
-            <div>
+            <div className="px-1.5 md:px-4 w-full">
               <Product
                 productImg={bestSellersOne}
                 badgeText={"New"}
@@ -38,7 +65,7 @@ const Bestsellers = () => {
               />
             </div>
              {/* ======== */}
-            <div>
+            <div className="px-1.5 md:px-4 w-full">
               <Product
                 productImg={bestSellersTwo}
                 badgeText={"New"}
@@ -47,7 +74,7 @@ const Bestsellers = () => {
               />
             </div>
              {/* ======== */}
-            <div>
+            <div className="px-1.5 md:px-4 w-full">
               <Product
                 productImg={bestSellersThree}
                 badgeText={"70%"}
@@ -56,7 +83,7 @@ const Bestsellers = () => {
               />
             </div>
              {/* ======== */}
-            <div>
+            <div className="px-1.5 md:px-4 w-full">
               <Product
                 productImg={bestSellersFour}
                 badgeText={"New"}
@@ -65,7 +92,7 @@ const Bestsellers = () => {
               />
             </div>
              {/* ======== */}
-            <div>
+            <div className="px-1.5 md:px-4 w-full">
               <Product
                 productImg={bestSellersTwo}
                 badgeText={"20%"}
@@ -74,7 +101,7 @@ const Bestsellers = () => {
               />
             </div>
              {/* ======== */}
-            <div>
+            <div className="px-1.5 md:px-4 w-full">
               <Product
                 productImg={bestSellersThree}
                 badgeText={"New"}
