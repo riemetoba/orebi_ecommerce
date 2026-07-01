2f99d084d01dd908af6dd46d6060326efe6245ae
Toba <riemehasan06@gmail.com>
2026-07-01 17:30:13 +0600

    style: make footer section responsive for all devices

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -8,11 +8,11 @@ import { FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="bg-[#F5F5F3] py-9 sm:py-[50px] mt-[100px]">
+    <div className="bg-[#F5F5F3] py-9 sm:py-[50px] mt-[60px] md:mt-[100px]">
       <Container>
-        <div className={"flex lg:justify-between items-start gap-x-2 sm:gap-x-7"}>
-          <div className="w-[40%]">
-            <Flex className={"gap-x-2 sm:gap-x-10 lg:justify-between items-start"}>
+        <div className="flex flex-col md:flex-row md:justify-between items-start gap-y-8 md:gap-y-0 gap-x-2 sm:gap-x-7 px-4 md:px-0">
+          <div className="w-full md:w-[40%]">
+            <Flex className="gap-x-4 sm:gap-x-10 justify-between items-start">
               <div className="">
                 <h4 className="font-DM font-bold text-[12px] sm:text-base">MENU</h4>
                 <ul>
@@ -75,34 +75,34 @@ const Footer = () => {
               </div>
             </Flex>
           </div>
-          <div className="w-[30%] ml-4 mb-28">
-            <h4 className="text-[10px] font-DM font-bold sm:text-sm lg:text-base">(052) 611-5711</h4>
-            <h4 className="text-[10px] font-DM font-bold sm:text-sm lg:text-base">company@domain.com</h4>
-            <p className="font-DM text-[10px] lg:text-sm text-halkaGray mt-5">
+          <div className="w-full md:w-[30%] mb-4 md:mb-28">
+            <h4 className="text-[12px] font-DM font-bold sm:text-sm lg:text-base">(052) 611-5711</h4>
+            <h4 className="text-[12px] font-DM font-bold sm:text-sm lg:text-base">company@domain.com</h4>
+            <p className="font-DM text-[11px] lg:text-sm text-halkaGray mt-2 md:mt-5">
               575 Crescent Ave. Quakertown, PA 18951
             </p>
           </div>
-          <div className="w-15 sm:w-20 lg:w-[30%]">
-            <Image src={logoFoooter} className={"mb-[150px]"} />
+          <div className="w-24 sm:w-28 md:w-[30%] flex md:justify-end">
+            <Image src={logoFoooter} className="mb-6 md:mb-[150px]" />
           </div>
         </div>
-        <Flex className={'gap-x-4 sm:gap-x-20 lg:justify-between'}>
-          <div className="flex gap-x-4 mt-12">
-                  <div className="">
-                    <FaFacebookF />
-                  </div>
-                  <div className="">
-                    <FaLinkedin />
-                  </div>
-                  <div className="">
-                    <FaInstagram />
-                  </div>
-                </div>
-                <div className="mt-12 text-right">
-                   <p className="font-DM text-[8px] sm:text-sm text-halkaGray">
+        <Flex className="flex-col-reverse sm:flex-row gap-y-4 sm:gap-y-0 gap-x-4 sm:gap-x-20 justify-between items-center md:items-start px-4 md:px-0">
+          <div className="flex gap-x-4 mt-6 sm:mt-12">
+            <div>
+              <FaFacebookF />
+            </div>
+            <div>
+              <FaLinkedin />
+            </div>
+            <div>
+              <FaInstagram />
+            </div>
+          </div>
+          <div className="mt-6 sm:mt-12 text-center sm:text-right">
+            <p className="font-DM text-[10px] sm:text-sm text-halkaGray">
               2020 Orebi Minimal eCommerce Figma Template by Adveits
             </p>
-                </div>
+          </div>
         </Flex>
       </Container>
     </div>
